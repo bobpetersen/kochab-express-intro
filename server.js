620abc7eb86944032ddebb49a120ec8cd394ada7
bobpetersen <33petersen@gmail.com>
2018-04-17 11:52:53 -0500

    added request recieve

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const express = require('express');// brings express in
+const express = require("express"); // brings express in
 
-const app = express();//  run express
+const app = express(); //  run express
 const PORT = 5000;
 
-app.listen(PORT);
+// app.get("/", function(req, res) {
+//   res.send("Hello World");
+// });
+
+// makes server listen for command
+app.listen(PORT, function() {
+  console.log(`listening on port: ${PORT}`);
+});
+
+// this will send you to localhost5000/hey
+app.get('/hey', function (req, res) {
+    res.send('/hey')
+});
